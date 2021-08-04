7f374792e2d7ad73427270be59421473286a42b9
followzhou <1692460871@qq.com>
2021-08-04 14:46:50 +0800

    second commit

--- src/main/java/com/rude3knife/commonutils/CommonUtilsApplication.java
+++ src/main/java/com/rude3knife/commonutils/CommonUtilsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CommonUtilsApplication {
 
     public static void main(String[] args) {
+        return 0;
         SpringApplication.run(CommonUtilsApplication.class, args);
     }
 
